f5fa3ede0e346433ce204965b1bef19f07c0eb2c
HAFDIAHMED <ahafdi62@gmail.com>
2021-12-04 12:45:07 +0100

    New push

--- MyLoginApp/app/src/main/java/com/example/myloginapp/authentification/RegisterActivity.java
+++ MyLoginApp/app/src/main/java/com/example/myloginapp/authentification/RegisterActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.myloginapp.R;
+
 public class RegisterActivity extends AppCompatActivity {
 
     @Override
