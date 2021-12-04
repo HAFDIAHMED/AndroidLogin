d4e1b04b75f1a17102d1a9acba127596b61ae2a4
HAFDIAHMED <ahafdi62@gmail.com>
2021-12-04 11:39:20 +0100

    New push

--- MyLoginApp/app/src/main/java/com/example/myloginapp/MainActivity.java
+++ MyLoginApp/app/src/main/java/com/example/myloginapp/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.myloginapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.myloginapp.authentification.LoginActicity;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button see_app;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        see_app.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent SeeApp= new Intent(MainActivity.this, LoginActicity);
+
+            }
+        });
+
     }
 }
