36fae6e7766f930cf0c3117e22ea13823b089d2c
HAFDIAHMED <ahafdi62@gmail.com>
2021-12-04 11:49:44 +0100

    New push

--- MyLoginApp/app/src/main/java/com/example/myloginapp/MainActivity.java
+++ MyLoginApp/app/src/main/java/com/example/myloginapp/MainActivity.java
@@ -6,22 +6,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.myloginapp.authentification.LoginActicity;
 
 public class MainActivity extends AppCompatActivity {
     Button see_app;
+    int counter=0;
+    TextView CounterText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         see_app = findViewById(R.id.button2);
+        CounterText=findViewById(R.id.textcountr);
         see_app.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //Intent SeeApp= new Intent(MainActivity.this, LoginActicity.class);
                 //startActivity(SeeApp);
-                see_app.setText("button test");
+                //counter=counter +1;
+                see_app.setText(counter);
 
             }
         });
