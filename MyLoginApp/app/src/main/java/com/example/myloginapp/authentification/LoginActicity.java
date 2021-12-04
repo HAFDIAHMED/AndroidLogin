8bde0bf64a437df14d14a46ab1d5a307838b2088
HAFDIAHMED <ahafdi62@gmail.com>
2021-12-04 12:03:32 +0100

    New push

--- MyLoginApp/app/src/main/java/com/example/myloginapp/authentification/LoginActicity.java
+++ MyLoginApp/app/src/main/java/com/example/myloginapp/authentification/LoginActicity.java
@@ -1,5 +1,6 @@
 package com.example.myloginapp.authentification;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -12,5 +13,9 @@ public class LoginActicity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_acticity);
+        //ActionBar actionBar = getSupportActionBar();
+        //actionBar.setDisplayHomeAsUpEnabled(true);
+
+
     }
 }
