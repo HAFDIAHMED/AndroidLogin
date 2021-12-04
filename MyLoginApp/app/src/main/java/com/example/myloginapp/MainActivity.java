b4a7a076800890c9ad56afe00abb6299214e5e2a
HAFDIAHMED <ahafdi62@gmail.com>
2021-12-04 15:04:27 +0100

    New push

--- MyLoginApp/app/src/main/java/com/example/myloginapp/MainActivity.java
+++ MyLoginApp/app/src/main/java/com/example/myloginapp/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 
 import com.example.myloginapp.authentification.LoginActicity;
 
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 import java.io.IOException;
@@ -83,6 +84,9 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        //Post Data
+        JSONObject postData = new JSONObject();
+        
 
 
 
