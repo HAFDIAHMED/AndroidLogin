db02f3896a8f82c5d5edd904442d54b881273603
HAFDIAHMED <ahafdi62@gmail.com>
2021-12-04 11:34:06 +0100

    New push

---
+++ MyLoginApp/app/src/main/java/com/example/myloginapp/authentification/LoginActicity.java
@@ -0,0 +1,14 @@
+package com.example.myloginapp.authentification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActicity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_acticity);
+    }
+}
